b873622c5fe13443d488a257aadbd0b98a5980d6
Wico Pretorius <wico.pretorius@jager.cloud>
2019-02-19 16:13:14 +0200

    Change port to 8080

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 const bodyParser = require('body-parser');
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8080;
 
 app.use(express.json());
 //app.use(bodyParser.urlencoded({ extended: true }));
